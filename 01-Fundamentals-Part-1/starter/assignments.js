ed2e0462df9b3702f076bc001ec6ae462db9441f
KOBEsphinx242 <ado.coneo@gmail.com>
2022-06-03 23:47:52 -0500

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,4 @@
+let country = "Colombia";
+let continent = "South America";
+let population = "50.88 million";
+console.log(country, continent, population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let js = 'amazing';
+console.log(48 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let myFirstJob = "Web Developer";
+let myCurrentJob = "Web Developer";
