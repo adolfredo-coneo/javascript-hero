05f27d6b64f17832d531560fa0ed9ebb5a3d7978
KOBEsphinx242 <ado.coneo@gmail.com>
2022-06-05 22:15:44 -0500

    more coding

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,4 +1,22 @@
-let country = "Colombia";
-let continent = "South America";
-let population = "50.88 million";
+const country = "Colombia";
+const continent = "South America";
+let population = 50088000;
 console.log(country, continent, population);
+
+const isIsland = false;
+let language;
+console.log(typeof country, typeof continent, typeof population, typeof isIsland, typeof language);
+
+language = "Spanish";
+
+console.log(population / 2);
+population++;
+console.log(population);
+
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+console.log(population < 33000000);
+const description = country + " is in " + continent + ", and its " + population + " people speak " + language;
+const descriptionWithLiteral = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(description);
+console.log(descriptionWithLiteral);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,75 @@
 let js = 'amazing';
-console.log(48 + 8 + 23 - 10);
+// console.log(48 + 8 + 23 - 10);
 
-let firstName = "Jonas";
-console.log(firstName);
+// console.log(firstName);
 
 let myFirstJob = "Web Developer";
-let myCurrentJob = "Web Developer";
+let $myCurrentJob = "Web Developer";
+
+//The 7 primitive data types that JavaScript supports are:
+//Number let myAge = 30;
+//String let myName = "Jonas";
+//Boolean let isMarried = false;
+//Undefined value taken by a variable that is not yet defined (empty value) let myJob;
+//Null also means empty value let myJob = null;
+//Symbol (ES2015): Value that is unique and cannot be changed, let myJob = Symbol("Web Developer");
+//BigInt (ES2020): Larger integers than Number type can't hold, let myAge = BigInt(30);
+
+//In JavaScript, value has type, not variable
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// javascriptIsFun = 100;
+// console.log(typeof javascriptIsFun);
+// let isUndefined;
+// console.log(isUndefined);
+// isUndefined = "Hello";
+// console.log(typeof isUndefined);
+// console.log(typeof null);
+
+
+// const birthYear = 1990;
+// var job = "Web Developer"; //old way
+// job = "Software Engineer";
+// secondName = "John";
+// console.log(birthYear, job, secondName);
+
+//Operators
+//Allows to transform values or combine values to create new values
+
+
+//Arithmetic Operators
+// const currentYear = 2037;
+// const ageJonas = currentYear - 1991;
+// const ageJohn = currentYear - 2018;
+// console.log(ageJonas, ageJohn);
+// console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+// // 2 ** 3 = 2 * 2 * 2 = 8
+// const firstName = "John";
+// const lastName = "Doe";
+// const fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+//Asignment operators
+// let x = 10 + 5;
+// x+= 10;
+// x++;
+// x--;
+// x*= 2;
+// x/= 2;
+// console.log(x);
+
+// //Comparison operators
+// console.log(ageJonas > ageJohn);
+// console.log(ageJohn >= 20);
+
+console.log("****************");
+const currentYear = 2037;
+const ageJonas = currentYear - 1991;
+const ageJohn = currentYear - 2018;
+console.log(currentYear - 1991 > currentYear - 2018);
+console.log("****************");
+
+
