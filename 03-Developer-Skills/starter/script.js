d0b1d0ea90951e44228daf66689c883e28254253
KOBEsphinx242 <ado.coneo@gmail.com>
2022-06-21 23:43:39 -0500

    developers skill

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log("11111");
+const array=[1,2,3,4,5,6,7,8,9,10];
+//npm install live-server -g
