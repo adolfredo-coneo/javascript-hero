2e3bc4f2e1f193768f370e8310119c6f4dcf4778
KOBEsphinx242 <ado.coneo@gmail.com>
2022-06-28 23:51:51 -0500

    modal project 2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtns = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', showModal);
+}
+
+closeModalBtn.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+    hideModal();
+  }
+});
+
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function hideModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
